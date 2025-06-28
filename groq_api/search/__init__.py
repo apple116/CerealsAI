a146bea321d7e82023b42a049f99eeedf2ad84c1
CerealsHub <89826328+apple116@users.noreply.github.com>
2025-06-28 15:36:17 +0700

    Delete groq_api directory

--- groq_api/search/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Search functionality including detection and DuckDuckGo integration.
-"""
-
-from .search_detector import should_perform_search
-from .duckduckgo_search import perform_search_and_summarize
-
-__all__ = ["should_perform_search", "perform_search_and_summarize"]
--- groq_api/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions and import handling.
-"""
-
-from .imports import setup_imports
-from .helpers import format_date, truncate_text
-
-__all__ = ["setup_imports", "format_date", "truncate_text"]
